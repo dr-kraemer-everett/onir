d9dd8a23f2b02d143781616b430fef5c1a11c6ae
dr. kraemer <jkraemer@everett.k12.ma.us>
2026-02-22 10:08:55 -0500

    (follow the 'channel-numbered-pointers' pattern in Control.)

--- src/control.cc
+++ src/control.cc
@@ -2,19 +2,21 @@
 
 #include "Arduino.h"
 
-Control::Control(int* ch, int count) {
-  channels = ch;
+
+Control::Control(int* channels, int ct) {
+  count_ = ct;
   for (int i = 0; i < BANDS; i++) {
-    clients[i] = nullptr;
+    clients[i] = 0;
   }
 
-  for (int i = 0; i < count; i++) {
-    clients[channels[i]] = new Client(channels[i]);
+  for (int i = 0; i < count_; i++) {
+    int channel = channels[i];
+    clients[channel] = new Client(channel);
   }
 }
 
 void Control::update() {
-    for (Client* client : clients) {
+  for (Client* client : clients) {
     if (client) {
       client->update();
     }
--- src/control.h
+++ src/control.h
@@ -1,15 +1,21 @@
+#pragma once
+
 #include "client.h"
 
-#define BANDS 16
+#define BANDS 16      // first eight spots unused. (https://i2cdevices.org/addresses)
+#define LOCAL_BAND 0  // zero = here  
 
 class Control {
 public:
   Control(int* channels, int count);
+
   void update();
+  int count() {return count_;}
+
   Client* clients[BANDS];
-  
+ 
 private:
-  int* channels = nullptr;
+  int count_ = 0;
 };
 
 
