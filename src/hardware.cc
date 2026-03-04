c9913c34c6060f6f02608fa3be9866f3ba5f8f99
dr. kraemer <jkraemer@everett.k12.ma.us>
2026-03-04 13:36:16 -0500

    build fix

--- src/hardware.cc
+++ src/hardware.cc
@@ -7,7 +7,7 @@ int assign(Hardware hardware, Function fn, int pin) {
 }
 
 bool empty(const Hardware& hardware) {
-  for (int fn = (int)Function::NONE + 1; fn < FUNCTION::COUNT; fn++) {  // skip empty hardware.
+  for (int fn = (int)Function::NONE + 1; fn < (int)Function::COUNT; fn++) {  // skip empty hardware.
     if (hardware[fn] > 0) {
       return false;  // found a good value
     }
